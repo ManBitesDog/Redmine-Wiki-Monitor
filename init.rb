46837372d2456217602c9d80bdd87c527aab76b0
ManBitesDog <samsee@live.co.kr>
2015-05-15 19:35:18 +0900

    Plugin generated
    Initial

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_wiki_monitor do
+  name 'Redmine Wiki Monitor plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
